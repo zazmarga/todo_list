7b4fbec0b6325e518398d462b2f33bdbaa990d82
Marharyta Zazulia <zazmargo@gmail.com>
2024-11-28 19:05:50 -0300

    register models in admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Tag, Task
+
+admin.site.register(Tag)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("done", "content", "finish_time", "get_tags", "created_time", )
+    search_fields = ("content",)
+    list_filter = ("done", )
+
+    def get_tags(self, obj):
+        return ", ".join(
+            [tag.name for tag in obj.tags.all()]
+        )
+
+    get_tags.short_description = "Tags"
